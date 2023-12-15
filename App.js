c2b82356f28ca4fd87e1c3ca31017deeb15a8cfa
uttam pun <uttampun62@gmail.com>
2023-12-15 22:51:20 +0545

    forgot password and signup screen done

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from './screen/Home';
 import Login from './screen/Login';
+import Signup from './screen/Signup';
+import Forgotpassword from './screen/Forgotpassword';
 
 export default function App() {
 
@@ -20,6 +22,14 @@ export default function App() {
             component={Login}
             name='Login'
             options={{headerShown:false}}
+           />
+           <Stack.Screen
+            component={Signup}
+            name='Signup'
+           />
+           <Stack.Screen
+            component={Forgotpassword}
+            name='Forgotpassword'
            />
         </Stack.Navigator>
      </NavigationContainer>
--- screen/Login.js
+++ screen/Login.js
@@ -1,49 +1,95 @@
+import React from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
-import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, Image, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default function App() {
+  const navigation = useNavigation();
 
-      const navigation = useNavigation()
+  const inputValue = {
+    email: '',
+    password: ''
+  };
 
-    const signup = () => {
-      Alert.alert("Welcome to signup screen")
+  const [inputVal, setVal] = useState(inputValue);
+
+  const signup = () => {
+    navigation.navigate('Signup');
+  };
+
+  // login button function
+  const login = () => {
+
+    if (inputVal.email.includes('@') === '') {
+
+     setVal(inputVal)
+    } else if (inputVal.password === '') {
+
+      setVal(inputVal)
+    } else {
+
+      alert('Valid login credentials. Logging in...');
     }
+  };
+
+  // onchange input field
+  const loginInput = (name) => (text) => {
+    setVal({ ...inputVal, [name]: text });
+  };
+
+  // forgot password
+  const password = () => {
+    navigation.navigate('Forgotpassword');
+  };
 
   return (
     <View style={loginStyle.container}>
       <Image source={require('../assets/images/login.png')} style={loginStyle.loginBg} />
       <View style={loginStyle.buttonContainer}>
         <Text style={loginStyle.loginHeading}>Get Started with Lobster</Text>
-        <Image source={require('../assets/images/close.png')} style={loginStyle.icon} />
 
         <View style={loginStyle.signupText}>
-          <Text style={loginStyle.signupText.firstText}>Don't have an account?</Text>
-          <Text style={loginStyle.signupText.secondText} onPress={signup}>Signup Now</Text>
+          <Text style={loginStyle.signupTextText}>Don't have an account?</Text>
+          <Text style={loginStyle.signupTextLink} onPress={signup}>
+            Signup Now
+          </Text>
         </View>
 
         {/* login inputfield */}
         <View>
-          <TextInput placeholder='Email or Phone Number' style={loginStyle.inputField} />
-          <TextInput placeholder='Enter Password' style={loginStyle.inputField} />
-          <Text style={loginStyle.forgotPassword}>Forgot Password ?</Text>
+          <TextInput
+            placeholder='Email or Phone Number'
+            style={loginStyle.inputField}
+            value={inputVal.email}
+            onChangeText={loginInput('email')}
+          />
+
+          {inputVal.email &&  <Text style={loginStyle.errorText}>Please fill the email or phone number</Text>}
+
+          <TextInput placeholder='Enter Password' style={loginStyle.inputField} value={inputVal.password} secureTextEntry={true} onChangeText={loginInput('password')} />
+
+          {inputVal.password && <Text style={loginStyle.errorText}>Please fill the password</Text>}
+          <Text style={loginStyle.forgotPassword} onPress={password}>
+            Forgot Password ?
+          </Text>
+
         </View>
 
         {/* login button */}
         <View>
-            <TouchableOpacity style={loginStyle.loginButton.button}>
-                <Text style={loginStyle.loginButton.text}>Log In</Text>
-            </TouchableOpacity>
-            <Text style={loginStyle.continue}>Or Continue</Text>
+          <TouchableOpacity style={loginStyle.loginButton} onPress={login}>
+            <Text style={loginStyle.loginButtonText}>Log In</Text>
+          </TouchableOpacity>
+          <Text style={loginStyle.continue}>Or Continue</Text>
         </View>
 
-   {/* facebooki login */}
+        {/* facebook login */}
         <View>
-            <TouchableOpacity style={loginStyle.facebook.button}>
-                <Icon name='facebook' size={20} color='#FFF' />
-                <Text style={loginStyle.facebook.text}>continue with facebook</Text>
-            </TouchableOpacity>
+          <TouchableOpacity style={loginStyle.facebookButton}>
+            <Icon name='facebook' size={20} color='#FFF' />
+            <Text style={loginStyle.facebookButtonText}>Continue with Facebook</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -52,7 +98,7 @@ export default function App() {
 
 const loginStyle = StyleSheet.create({
   container: {
-    backgroundColor: "#FFD7CC",
+    backgroundColor: '#FFD7CC',
     flex: 1,
     position: 'relative'
   },
@@ -60,7 +106,7 @@ const loginStyle = StyleSheet.create({
     width: 330,
     height: 330,
     resizeMode: 'contain',
-    marginLeft: 25,
+    marginLeft: 25
   },
   buttonContainer: {
     backgroundColor: '#fff',
@@ -70,30 +116,28 @@ const loginStyle = StyleSheet.create({
     top: 150,
     width: '100%',
     zIndex: 99,
-    borderRadius: 30,
+    borderRadius: 30
   },
   loginHeading: {
     fontSize: 32,
     fontWeight: 'bold',
     marginTop: 60,
     marginBottom: 2,
-    paddingHorizontal: 16,
-  },
-  icon: {
-    width: 24,
-    height: 24,
-    resizeMode: 'contain',
-    position: 'absolute',
-    right: 20,
-    top: 30,
-    borderRadius: 50,
-    backgroundColor: '#9ca3af',
+    paddingHorizontal: 16
   },
   signupText: {
-    flex: 1,
     flexDirection: 'row',
     paddingTop: 10,
-    alignItems: 'center',
+    alignItems: 'center'
+  },
+  signupTextText: {
+    color: '#8C9099',
+    fontWeight: '400',
+    marginRight: 16
+  },
+  signupTextLink: {
+    color: '#323643',
+    fontWeight: '700'
   },
   inputField: {
     height: 40,
@@ -103,70 +147,54 @@ const loginStyle = StyleSheet.create({
     paddingHorizontal: 16,
     marginHorizontal: 16,
     marginVertical: 10,
-    borderRadius: 12,
-
+    borderRadius: 12
   },
-  forgotPassword:{
+  errorText: {
+    color: '#FF0000',
+    marginLeft: 16
+  },
+  forgotPassword: {
     textAlign: 'right',
     paddingRight: 16
- },
-  signupText: {
-    flexDirection: 'row',
-    marginHorizontal: 16,
-    marginTop: 20,
-
-  firstText: {
-    color: '#8C9099',
-    fontWeight: '400',
-    marginRight: 16,
   },
-  secondText: {
-    color: '#323643',
-    fontWeight: 700,
-  }
- },
- loginButton:{
-     button:{
-        marginVertical: 20,
-        backgroundColor: '#FE724C',
-        padding: 15,
-        width: '90%',
-        paddingHorizontal: 20,
-        borderRadius: 12,
-        marginHorizontal: 16,
-     },
-    text:{
-        textAlign: 'center',
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: '600',
-    }
- },
- continue:{
+  loginButton: {
+    marginVertical: 20,
+    backgroundColor: '#FE724C',
+    padding: 15,
+    width: '90%',
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    marginHorizontal: 16
+  },
+  loginButtonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600'
+  },
+  continue: {
     textAlign: 'center',
     fontWeight: '400',
     fontSize: 16,
     color: '#323643'
- },
- facebook:{
-    button:{
-        marginVertical: 20,
-        backgroundColor: '#1877F2',
-        padding: 15,
-        width: '90%',
-        paddingHorizontal: 20,
-        borderRadius: 12,
-        marginHorizontal: 16,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        columnGap: 10
-     },
-    text:{
-        textAlign: 'center',
-        color: '#fff',
-        fontSize: 18,
-        fontWeight: '600',
-    }
- }
+  },
+  facebookButton: {
+    marginVertical: 20,
+    backgroundColor: '#1877F2',
+    padding: 15,
+    width: '90%',
+    paddingHorizontal: 20,
+    borderRadius: 12,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    columnGap: 10
+  },
+  facebookButtonText: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: '600'
+  }
 });
